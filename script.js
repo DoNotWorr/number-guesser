8936d42a038b16ec7c246a7a9899d87a052ea57a
Alexander Wiklund <34245732+DoNotWorr@users.noreply.github.com>
2021-08-24 13:22:47 +0200

    generate target method

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+/**
+ * @returns Random number between 0 and 9
+ */
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+}
