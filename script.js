5f3c991ee4327b2fe3fcda752ebc4ab1d1f58ad6
Alexander Wiklund <34245732+DoNotWorr@users.noreply.github.com>
2021-08-24 13:27:47 +0200

    Added compare guesses function

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
     return Math.floor(Math.random() * 10)
 }
+
+const compareGuesses = (playerGuess, computerGuess, target) => {
+    let playerDelta = Math.abs(target - playerGuess);
+    let computerDelta = Math.abs(target - computerGuess);
+    return playerDelta <= computerDelta
+}
